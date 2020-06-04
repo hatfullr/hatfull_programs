5020e06d32ca03fb3867c36fbe2f7f59a6d9e889
hatfullr <hatfull@ualberta.ca>
2020-06-04 17:14:43 -0600

    Added plot_Nnb_compare.py

---
+++ plot_Nnb_compare.py
@@ -0,0 +1,51 @@
+from glob import glob
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+plt.style.use('supermongo')
+
+if len(sys.argv) > 1:
+    user_input = " ".join(sys.argv[1:])
+else:
+    user_input = raw_input("Enter file name(s) or pattern(s): ")
+
+user_input = user_input.split(" ")
+
+files = []
+for f in user_input:
+    for f1 in sorted(glob(f)):
+        files.append(f1)
+
+for f in files:
+    if f.split("/")[-1][:3] != "col" and f.split("/")[-1][-3:] != ".sph":
+        raise Exception("File '"+f+"' does not match the file name requirements. Must use a file that begins with 'col' and ends with '.sph'")
+
+fig, ax = plt.subplots(dpi=200)
+
+for f in files:
+    data = np.loadtxt(f)
+    r = data[:,0]
+    Nnb = data[:,7]
+    u = data[:,14]
+
+    core_idx = np.where(u==0)[0]
+    idx = np.arange(len(data))
+
+    label = f[-15:]
+    
+    if len(core_idx) == 1:
+        sc = ax.scatter(r[idx != core_idx],Nnb[idx != core_idx],s=1,label=label)
+        c = sc.get_facecolors()
+        ax.scatter(r[core_idx],Nnb[core_idx],s=50,facecolors='none',edgecolors=c)
+    else:
+        ax.scatter(r,Nnb,s=1,label=label)
+
+ax.legend()
+        
+ax.set_xlabel("$r_i$ [$R_\\odot$]")
+ax.set_ylabel("$N_{nb,i}$")
+
+plt.savefig("Nnb_vs_r_compare.png")
+
+plt.show()
